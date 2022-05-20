e3d8943c46061b42be9bc9fcad8263b2c906f1bb
hark130 <hark130@yahoo.com>
2022-05-20 17:24:08 -0500

    LIDE-13: Polished up Challenge 201 solution
    Sure, it's currently in the challenge directory... but this will
    *become* the solution.
    
    Added better comments.  Polished the challenge requirements.
    
    Changes to be committed:
    	modified:   exercises/2-Kernel_Mode/challenge_201/201_source.c

--- exercises/2-Kernel_Mode/challenge_201/201_source.c
+++ exercises/2-Kernel_Mode/challenge_201/201_source.c
@@ -1,22 +1,46 @@
-/*  
- *  201_source.c - The simplest kernel module.
+/*
+ * Filename: 201_source.c
+ * Brief: All exercise code for Kernel Mode challenge 201 goes here.
+ * Details:
+ *     - Write "the simplest kernel module"
+ *     - Define init_module() to:
+ *         - Print "challenge_201: Loading" to the kernel log buffer
+ *         - Use the "Notice" log level
+ *     - Define cleanup_module() to:
+ *         - Print "challenge_201: Unloading" to the kernel log buffer
+ *         - Use the "Notice" log level
+ *     - Feel free to use the MODULE_NAME macro to assist with message formatting
  */
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/module.h>
+#include <linux/kernel.h>
 
+#define MODULE_NAME "challenge_201: "  // Standard method of identifying the source of log messages
+
+
+/*
+ * Function: init_module
+ * Description: Expected default name for the kernel module "start" (initialization) function.
+ *     This function is called when the module is insmoded into the kernel.
+ */
 int init_module(void)
 {
-	printk(KERN_INFO "Hello world\n");
-
-	/* 
-	 * A non 0 return means init_module failed; module can't be loaded. 
-	 */
-	return 0;
+    // PRINT OPTION 1: printk() messages are printed to the kernel log buffer, which is a ring
+    //  buffer exported to userspace through /dev/kmsg.  printk() messages are printed to the
+    //  kernel log buffer, which is a ring buffer exported to userspace through /dev/kmsg.
+    printk(KERN_NOTICE "%s%s", MODULE_NAME, "Loading\n");
+    return 0;  // A non-zero return means init_module failed; module can't be loaded. 
 }
 
+
+/*
+ * Function: cleanup_module
+ * Description: Expected default name for the kernel module "end" (cleanup) function.
+ *     This function is called just before the module is rmmoded.
+ */
 void cleanup_module(void)
 {
-	printk(KERN_INFO "Goodbye world\n");
+    // PRINT OPTION 2: linux/printk.h defines a collection of MACROS to print at certain log levels.
+    pr_notice("%s%s", MODULE_NAME, "Unloading\n");
 }
 
 
