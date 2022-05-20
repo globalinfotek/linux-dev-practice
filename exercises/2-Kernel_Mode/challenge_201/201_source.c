96cb6caad9d8ac1e15d2d272305fea540973ca5e
hark130 <hark130@yahoo.com>
2022-05-20 14:10:05 -0500

    LIDE-13: Challenge 201 source file Initial Commit
    Changes to be committed:
    	new file:   exercises/2-Kernel_Mode/challenge_201/201_source.c

---
+++ exercises/2-Kernel_Mode/challenge_201/201_source.c
@@ -0,0 +1,25 @@
+/*  
+ *  201_source.c - The simplest kernel module.
+ */
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+
+int init_module(void)
+{
+	printk(KERN_INFO "Hello world\n");
+
+	/* 
+	 * A non 0 return means init_module failed; module can't be loaded. 
+	 */
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world\n");
+}
+
+
+// Descriptive information about this module
+MODULE_DESCRIPTION("Linux Dev Practice Challenge 201");
+MODULE_LICENSE("GPL");  // Required
